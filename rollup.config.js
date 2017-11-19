2ad862683e46663cf20e9bc5d3229b020f41f682
Phil Plückthun <phil@plckthn.me>
2017-11-19 21:39:20 +0000

    Add babel-plugin-closure-elimination to rollup dist build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,16 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify-es';
 
 const plugins = [
   nodeResolve(),
   commonjs({
     ignoreGlobal: true,
+  }),
+  babel({
+    babelrc: false,
+    plugins: ['babel-plugin-closure-elimination']
   })
 ];
 
